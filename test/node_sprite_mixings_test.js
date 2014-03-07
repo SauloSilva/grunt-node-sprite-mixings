4e1e8078acc0e4f83c1170d8ffe1d7072b01bf08
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2014-03-06 21:14:23 -0300

    new feature with and height

--- test/node_sprite_mixings_test.js
+++ test/node_sprite_mixings_test.js
@@ -6,7 +6,7 @@ var grunt = require('grunt'),
 
 exports.node_sprite_mixings = {
     global: function(test) {
-        test.expect(19);
+        test.expect(33);
         var pathStyl = './test/expected/global.styl',
             pathJson = './test/fixtures/',
             jsons = ['bar.json', 'foo.json', 'example.json'],
@@ -14,23 +14,30 @@ exports.node_sprite_mixings = {
 
         jsons.forEach(function(json) {
             var jsonParsed = grunt.file.readJSON(pathJson + json)
-
             test.equal(grunt.file.exists(pathStyl), true, 'Should file exist')
-            test.notEqual(globalStyl.indexOf(_.first(jsonParsed.images).name), -1, 'Should contain the name of mixing, the same json of the file');
-            test.notEqual(globalStyl.indexOf(_.last(jsonParsed.images).name), -1, 'Should contain the name of mixing, the same json of the file');
-            test.notEqual(globalStyl.indexOf('background'), -1, 'Should contain the background of mixing');
             test.notEqual(globalStyl.indexOf("url('" + urlNamespace + jsonParsed.name + '-' + jsonParsed.shortsum + ".png')"), -1, 'Should contain the url of mixing');
+
+            jsonParsed.images.forEach(function(json) {
+                test.notEqual(globalStyl.indexOf(json.name), -1, 'Should contain the name of mixing, the same json of the file');
+                test.notEqual(globalStyl.indexOf(json.positionY), -1, 'Should contain the positionY of mixing, the same json of the file');
+                test.notEqual(globalStyl.indexOf(json.positionX), -1, 'Should contain the positionX of mixing, the same json of the file');
+                test.notEqual(globalStyl.indexOf(json.width), -1, 'Should contain the width of mixing, the same json of the file');
+                test.notEqual(globalStyl.indexOf(json.height), -1, 'Should contain the height of mixing, the same json of the file');
+            })
         })
 
-        test.notEqual(globalStyl.indexOf('repeat'), -1, 'Should contain the repeat of mixing');
+        test.notEqual(globalStyl.indexOf('background'), -1, 'Should contain the background of mixing');
+        test.notEqual(globalStyl.indexOf('width'), -1, 'Should contain the width of mixing');
+        test.notEqual(globalStyl.indexOf('height'), -1, 'Should contain the width of mixing');
         test.notEqual(globalStyl.indexOf('y-offset'), -1, 'Should contain the y-offset of mixing');
         test.notEqual(globalStyl.indexOf('x-offset'), -1, 'Should contain the y-offset of mixing');
+        test.notEqual(globalStyl.indexOf('repeat'), -1, 'Should contain the repeat of mixing');
         test.notEqual(globalStyl.indexOf('transparent'), -1, 'Should contain the transparent of mixing');
         test.done();
     },
 
     basicAndExtra: function(test) {
-        test.expect(18);
+        test.expect(28);
         var pathStyl = './test/expected/',
             pathJson = './test/fixtures/',
             files = ['foo', 'bar'];
@@ -39,12 +46,19 @@ exports.node_sprite_mixings = {
             var styl = grunt.file.read(pathStyl + file + '.styl'),
                 jsonParsed = grunt.file.readJSON(pathJson + file + '.json');
 
-            test.notEqual(styl.indexOf(_.first(jsonParsed.images).name), -1, 'Should contain the name of mixing, the same json of the file');
-            test.notEqual(styl.indexOf(_.last(jsonParsed.images).name), -1, 'Should contain the name of mixing, the same json of the file');
-            test.notEqual(styl.indexOf('background'), -1, 'Should contain the background of mixing');
-            test.notEqual(styl.indexOf("url('" + urlNamespace + jsonParsed.name + '-' + jsonParsed.shortsum + ".png')"), -1, 'Should contain the url of mixing');
-
             test.equal(grunt.file.exists(pathStyl + file + '.styl'), true, 'Should file exist')
+            test.notEqual(styl.indexOf("url('" + urlNamespace + jsonParsed.name + '-' + jsonParsed.shortsum + ".png')"), -1, 'Should contain the url of mixing');
+            jsonParsed.images.forEach(function(json) {
+                test.notEqual(styl.indexOf(json.name), -1, 'Should contain the name of mixing, the same json of the file');
+                test.notEqual(styl.indexOf(json.positionX), -1, 'Should contain the positionX of mixing, the same json of the file');
+                test.notEqual(styl.indexOf(json.positionY), -1, 'Should contain the positionY of mixing, the same json of the file');
+                test.notEqual(styl.indexOf(json.width), -1, 'Should contain the width of mixing, the same json of the file');
+                test.notEqual(styl.indexOf(json.height), -1, 'Should contain the height of mixing, the same json of the file');
+
+            })
+            test.notEqual(styl.indexOf('background'), -1, 'Should contain the background of mixing');
+            test.notEqual(styl.indexOf('width'), -1, 'Should contain the width of mixing');
+            test.notEqual(styl.indexOf('height'), -1, 'Should contain the width of mixing');
             test.notEqual(styl.indexOf('repeat'), -1, 'Should contain the repeat of mixing');
             test.notEqual(styl.indexOf('y-offset'), -1, 'Should contain the y-offset of mixing');
             test.notEqual(styl.indexOf('x-offset'), -1, 'Should contain the y-offset of mixing');
